c05785b5b5a489f435a498efb1441e13b5411ac7
itTest <test@mail.ru>
2022-12-19 18:39:40 +0500

    minsize

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -19,6 +19,9 @@ namespace ConsoleApp15
             long maxmemo = 0;
             string maxmemo_process_name="";
             int maxmemo_process_id=0;
+            long minmemo = 11015555555;
+            string minmemo_process_name = "";
+            int minmemo_process_id = 0;
             foreach (Process process1 in Process.GetProcesses())
             {
                text+=($"ID: {process1.Id},NameProcess: {process1.ProcessName}, " +
@@ -30,13 +33,20 @@ namespace ConsoleApp15
                     maxmemo_process_name = process1.ProcessName;
                     maxmemo_process_id= process1.Id;
                 }
-                
+                if (process1.VirtualMemorySize64 < minmemo)
+                {
+                    minmemo = process1.VirtualMemorySize64;
+                    minmemo_process_name = process1.ProcessName;
+                    minmemo_process_id = process1.Id;
+                }
+
 
             }
             DateTime data = DateTime.Now;
             string path = data.Day.ToString()+"."+data.Month.ToString()+"."+data.Year.ToString()+"process.txt";
             File.AppendAllText(path, text);
             File.AppendAllText(path, $"Max memory process\n {maxmemo_process_id}\t{maxmemo_process_name}\t{maxmemo}");
+            File.AppendAllText(path, $"\nMin memory process\n {minmemo_process_id}\t{minmemo_process_name}\t{minmemo}");
 
 
 
