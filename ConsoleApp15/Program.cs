374924299af65db0b009009b6e91d1df7821d09e
itTest <test@mail.ru>
2022-12-21 18:40:27 +0500

    add thread info

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Dynamic;
 using System.Text;
 using System.IO;
+using System.Threading;
 
 namespace ConsoleApp15
 {
@@ -15,11 +16,16 @@ namespace ConsoleApp15
         public long id { get; set; }
         public string name { get; set; }
         public long memo { get; set; }
+        public ProcessThreadCollection threads { get; set; }
         public void Print()
         {
             Console.WriteLine($"Process ID:{this.id}");
             Console.WriteLine($"Process Name:{this.name}");
             Console.WriteLine($"Process Memmory size:{this.memo}");
+            foreach(ProcessThread thread in threads)
+            {
+                Console.WriteLine($"\tThread id:{thread.Id}\n\tPrioritet {thread.CurrentPriority}\n\t Start Adress{thread.StartAddress}");
+            }
         }
         internal class Program
         {
@@ -27,16 +33,18 @@ namespace ConsoleApp15
             {
                 
                 List<proc> procList = new List<proc>();
+                
                 foreach (Process pr in Process.GetProcesses())
                 {
                     proc a = new proc();
                     a.id = pr.Id;
                     a.name = pr.ProcessName;
-                    a.memo = pr.PagedMemorySize64;
+                    a.memo = pr.PagedMemorySize64/1024;
+                    a.threads = pr.Threads;
                     procList.Add(a);
                 }
-                
-                Console.WriteLine("Sort:\n1.Name\n2.ID\n3.Size");
+                string nm;
+                Console.WriteLine("Sort:\n1.Name\n2.ID\n3.Size\n4.Find process\n5.List and count");
                 int menu = int.Parse(Console.ReadLine());
                 switch (menu)
                 {
@@ -49,14 +57,36 @@ namespace ConsoleApp15
                     case 3:
                         procList.Sort(delegate (proc p1, proc p2) { return p1.memo.CompareTo(p2.memo); });
                         break;
-                    default:
-                        Console.WriteLine();
+                    case 4:
+                         Console.WriteLine("Введите имя процесса:");
+                        nm=Console.ReadLine();
+                        for (int i = 0; i < procList.Count; i++)
+                            if (nm == procList[i].name)
+                            {
+                                Console.WriteLine("YES");
+                                break;
+                            }
+
                         break;
+                    case 5:
+                        for (int i=0;i<procList.Count-1;i++)
+                        {
+                            int z = 0;
+                            for (int j=i+1;j<procList.Count;j++)
+                            {
+                                if (procList[i].name == procList[j].name)
+                                    z++;
+                            }
+                            Console.WriteLine($"Количество экземпляров: {procList[i].name} составляет: {z}");
+                        }
+                        break;
+
                 }
                 foreach (proc proc in procList)
                 {
                     proc.Print();
                 }
+
                 string text = "";
 
                 /*DateTime data = DateTime.Now;
